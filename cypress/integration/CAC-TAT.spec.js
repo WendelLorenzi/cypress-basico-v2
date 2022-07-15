208461229f1b9fde05454843b67ba18d87ebb087
WendelLorenzi <wlorenzi17@gmail.com>
2022-07-14 22:33:27 -0300

    feat: init firt test

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" /> 
+
+// describe é o switch de testes
+// a string do describe é a 
+describe('Central de atendimento ao cliente (TAT)', function() {
+    // it é o teste case
+    it('Verifica o titulo da aplicação', function() {
+
+    })
+})
