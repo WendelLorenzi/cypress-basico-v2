3e855f39d79a0c321e550efd743caa93902799b6
WendelLorenzi <wlorenzi17@gmail.com>
2022-07-20 23:39:24 -0300

    feat: test in filds type files

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -95,6 +95,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             cy.wrap($radio).check()
             cy.wrap($radio).should('be.checked')
         })
+    })
 
     it('marca ambos checkboxes, depois desmarca o último', function() {
         cy.get('input[type="checkbox"]')
@@ -104,5 +105,42 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('not.be.checked')
     })
 
+    it('seleciona um arquivo da pasta fixtures', function() {
+        cy.get('input[type="file"]')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json')
+        .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function() {
+        cy.get('input[type="file"]')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' } )
+        .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"')
+        .selectFile('@sampleFile')
+        .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+        cy.get('#privacy a').should('have.attr', 'target', '_blank')
+    })
+
+    it('acessa a página da política de privacidade removendo o target e então clicanco no link', function() {
+        cy.get('#privacy a')
+        .invoke('removeAttr', 'target')
+        .click()
+
+        cy.contains('Talking About Testing').should('be.visible')
     })
 })
